2a59b826ad220437cce4b65eed4031e272747e98
adrianoesch <adrianoesch@gmx.ch>
2017-09-05 12:13:54 +0100

    new features

--- funs.R
+++ funs.R
@@ -1,13 +1,19 @@
 # utility function collection
-aggregate2 = function(formula,df,fun=mean,new.names=NULL){
+aggregate2 = function(formula,df,fun=mean,new.names=NULL,metrics=c('mean','n','sd','se')){
   x = as.data.frame(aggregate(formula,df,fun))
   ncols = ncol(x)
   if(!is.null(new.names)){
     names(x) = new.names
   }
-  x$len = aggregate(formula,df,length)[[ncols]]
-  x$sd = aggregate(formula,df,sd)[[ncols]]
-  x$se = x$sd/sqrt(x$len)
+  if('n'%in%metrics){
+    x$n = aggregate(formula,df,length)[[ncols]]
+    if('sd' %in% metrics){
+      x$sd = aggregate(formula,df,sd)[[ncols]]    
+      if('se' %in% metrics){
+        x$se = x$sd/sqrt(x$len)     
+      }
+    }
+  }
   return(x)
 }
 
@@ -22,5 +28,15 @@ splitInTwo = function(variable){
   x = quantile(variable,c(0,.5,1))
   x[length(x)] = x[length(x)]*1.0000001
   x2 = cut(variable,breaks=x,labels=c('low','high'),right=F)
+  return(x2)
+}
+
+splitInX = function(variable,x,labels=NULL){
+  breaks = quantile(variable,seq(0,1,1/x))
+  breaks[length(breaks)] = breaks[length(breaks)]*1.0000001
+  if(is.null(labels)){
+    labels=paste0('g',seq(1,x))
+  }
+  x2 = factor(cut(variable,breaks=breaks,labels=labels,right=F),ordered=T,labels=labels)
   return(x2)
 }
